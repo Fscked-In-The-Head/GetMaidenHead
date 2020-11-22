ffb90099fb10a3ac242adb887c4ebc7e43e52044
Fscked-In-The-Head <Fscked-In-The-Head@users.noreply.github.com>
2020-11-21 19:59:48 -0800

    Added more letters
    Now supports 10 characters of accuracy

--- GetMaidenHead.js
+++ GetMaidenHead.js
@@ -1,6 +1,4 @@
-// Implementation based on N1SV's 8-character grid square calculation
-// http://n1sv.com/PROJECTS/How%20to%20calculate%20your%208-digit%20grid%20square.pdf
-getMaidenHead(lat,lon){
+function getMaidenHead(lat,lon){
   var U = 'ABCDEFGHIJKLMNOPQRSTUVWX';
   var L = U.toLowerCase();
   //Latitude
@@ -10,14 +8,20 @@ getMaidenHead(lat,lon){
   sqLat = Math.floor(t);
   t=(t-sqLat)*24;
   subLat = Math.floor(t);
-  extLat = Math.floor((t-subLat)*10);
-  //Longitude
+  t=(t-subLat)*10;
+  extLat = Math.floor(t);
+  t=(t-extLat)*24;
+  supLat = Math.floor(t);
   t = (lon+180)/20;
   fLon = Math.floor(t);
   t = (t % fLon)*10;
   sqLon = Math.floor(t);
   t=(t-sqLon)*24;
   subLon = Math.floor(t);
-  extLon = Math.floor((t-subLon)*10);
-  return U[fLon]+U[fLat]+sqLon+sqLat+L[subLon]+L[subLat]+extLon+extLat;
+  t=(t-subLon)*10;
+  extLon = Math.floor(t);
+  t=(t-extLon)*24;
+  supLon = Math.floor(t);
+  return U[fLon]+U[fLat]+sqLon+sqLat+L[subLon]+L[subLat]
+    +extLon+extLat+U[supLon]+U[supLat];
 }
